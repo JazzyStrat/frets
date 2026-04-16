2756c387d0e277974ec8fcdebbbbba7e05a402d2
JazzyStrat <jabbr@riseup.net>
2026-04-15 20:51:30 -0700

    added diatonic KEY MODE and actual modal for note select warning

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,9 @@
-import { triadMap, noteMapFlats, noteMapSharps } from './maps.js'
+import {
+    triadMap,
+    noteMapFlats,
+    noteMapSharps,
+    diatonicWheel,
+} from './/maps.js'
 
 const LAST_FRET = 15
 
@@ -6,7 +11,6 @@ const mainboard = []
 const freeboard = []
 
 const boards = [mainboard, freeboard]
-
 // for main board
 let tri = []
 
@@ -18,18 +22,23 @@ let fifth
 
 // dashboard
 let rootAlpha // just note letter as string
-let quality // maj, min, dim -- TODO: aug
+let quality // maj, min, dim aug
 let inversion
 
 let triadInfo = document.getElementById('triad-info')
+
+let keyMode = false
+let capturedRoot = null
+let wheelIdx
+let keyModeButton = document.getElementById('key-mode')
 
 // hit up the map, identify triad
 function calcTriadType() {
     const firstInterval = tri[1].abs - tri[0].abs
     const secInterval = tri[2].abs - tri[1].abs
     const intervals = `${firstInterval},${secInterval}`
-    // console.log(intervals)
-    const desc = triadMap[intervals]
+
+    const desc = triadMap.get(intervals)
     if (desc == null) {
         alert('rutro, unidentifiable triad')
         return null
@@ -37,10 +46,15 @@ function calcTriadType() {
 
     quality = desc.quality
     inversion = desc.inversion
+
+    // if (quality != 'maj' && keyMode) {
+    //     toggleKeyMode()
+    // }
     // find root index, set rootAlpha and root
     for (let z = 0; z < 3; z++) {
         if (z != desc.thirdIdx && z != desc.fifthIdx) {
             rootAlpha = tri[z].innerText
+            if (keyMode && wheelIdx == 0) capturedRoot = rootAlpha
             root = tri[z]
             break
         }
@@ -53,15 +67,15 @@ function calcTriadType() {
         // COMING SOON
         // rootAlpha += ` or ${tri[desc.thirdIdx].innerText} or ${fifth.innerText}`
     }
-    third.lastElementChild.classList.add('third-color')
-    fifth.lastElementChild.classList.add('fifth-color')
 
     tri.forEach((div) => {
         div.lastElementChild.classList.add('active')
     })
+    third.lastElementChild.classList.add('third-color')
+    fifth.lastElementChild.classList.add('fifth-color')
 
     alphaCheck(desc)
-    updateDash()
+    updateUI()
     return desc
 }
 
@@ -78,7 +92,6 @@ function alphaCheck(desc) {
     // A B C D E F G A                     // this breaks f dim alphas
     if ((distOne != 2 && distOne != 5) || (distTwo != 2 && distTwo != 5)) {
         switchSignsRedux()
-        // switchSigns()
         for (let z = 0; z < 3; z++) {
             if (z != desc.thirdIdx && z != desc.fifthIdx) {
                 rootAlpha = tri[z].innerText
@@ -130,7 +143,7 @@ function absToNote(note, flats) {
         }
 }
 
-// distance from nutZero to fret n
+// distance from nut to fret n
 function TwelfthRoot(n) {
     return 1 - Math.pow(2, -n / 12)
 }
@@ -141,13 +154,13 @@ function buildFretboard(boardID, stringsArr) {
 
     // calc fret widths
     const fretWidths = []
-    const visible = TwelfthRoot(LAST_FRET) // ~0.579
+    const relDist = TwelfthRoot(LAST_FRET) // ~0.579
 
     let prev = 0
     for (let n = 1; n <= LAST_FRET; n++) {
         const d = TwelfthRoot(n)
-        const segWidth = (d - prev) / visible // normalization
-        fretWidths.push(segWidth)
+        const fspan = (d - prev) / relDist
+        fretWidths.push(fspan)
         prev = d
     }
 
@@ -247,7 +260,6 @@ function buildFretboard(boardID, stringsArr) {
         })
     })
 
-    // single event delegation
     board.addEventListener('click', (e) => {
         const fretDiv = e.target.closest('.fw') || e.target.closest('.nut')
         // exclude highest playable note on fretboard
@@ -260,15 +272,17 @@ function buildFretboard(boardID, stringsArr) {
         }
     })
 }
+
 // lay as major triad
 function initTriad(fd) {
+    wheelIdx = 0
     clearTri()
     // E A D G strings
+    // 0th inversion
     if (fd.coord[0] < mainboard.length - 2) {
-        // 0th inversion
         const thirdPitch = fd.abs + 4
         const fifthPitch = fd.abs + 7
-        // proceedurally match
+        // proceedurally match targets
         for (let f = 0; f <= LAST_FRET; ++f) {
             if (mainboard[fd.coord[0] + 1][f].abs == thirdPitch) {
                 third = mainboard[fd.coord[0] + 1][f] // this is div
@@ -285,7 +299,7 @@ function initTriad(fd) {
                 break
             }
         }
-        // B string
+        // B string, 2nd inversion
     } else if (fd.coord[0] == 4) {
         // |E---------
         // |B---------
@@ -313,7 +327,6 @@ function initTriad(fd) {
         return
     }
     calcTriadType()
-    document.getElementById('instructions').style.opacity = 0
 }
 
 // sets triad given raw absolute pitch array and string offset
@@ -323,8 +336,9 @@ function shiftTriad(absPitches, offset) {
     let s = tri[0].coord[0] + offset // starting string = current + 0/1/-1
 
     if (s < 0 || s > 3) {
+        console.log('⚡️ unshiftable')
         glow()
-        return
+        return false
     }
 
     for (const pitch of absPitches) {
@@ -337,16 +351,20 @@ function shiftTriad(absPitches, offset) {
         }
     }
 
+    //TODO: evaluate neccesity of both conditions
     if (newTriad.length == 3 && !newTriad.includes(undefined)) {
         clearTri()
         tri = newTriad
         calcTriadType()
+        return true
     } else {
         console.log('⚡️ unshiftable')
         glow()
+        return false
     }
 }
 
+// indicate move not possible / current state
 function glow() {
     for (let note of tri) {
         note.lastElementChild.classList.add('glow')
@@ -375,17 +393,79 @@ function switchSignsRedux() {
 
 function invertTriadUp(lat = false) {
     let freqs = []
-    tri.forEach((div) => {
-        freqs.push(div.abs)
+    tri.forEach((note) => {
+        freqs.push(note.abs)
     })
+    // use map + array to get right freqs:
+    // triadMap and diatonicWheel
+    if (lat && keyMode) {
+        if (wheelIdx + 1 < diatonicWheel.length) {
+            wheelIdx++
+        } else {
+            wheelIdx = 0
+        }
 
-    let lp = freqs[0] // low pitch
-    freqs[0] = freqs[1]
-    freqs[1] = freqs[2]
-    freqs[2] = lp + 12
+        let nextQuality = diatonicWheel[wheelIdx].q
+        let nextIntervals
+        // backwards map key lookup
+        for (let [k, v] of triadMap) {
+            // inversion type the same for lateral changes
+            if (v.inversion == inversion && v.quality == nextQuality) {
+                nextIntervals = k
+                break
+            }
+        }
+        if (nextIntervals == null) {
+            glow()
+            return
+        }
+
+        const desc = calcTriadType()
+        let rootIdx = -1
+        for (let z = 0; z < 3; z++) {
+            if (z != desc.thirdIdx && z != desc.fifthIdx) {
+                rootIdx = z
+                break
+            }
+        }
+
+        // different span use based on inversion type
+        // stemming from root offset (os)
+        switch (inversion) {
+            case 'root position': {
+                freqs[0] += diatonicWheel[wheelIdx].os
+                freqs[1] = freqs[0] + Number(nextIntervals[0])
+                freqs[2] = freqs[1] + Number(nextIntervals[2])
+                break
+            }
+            case '1st inversion': {
+                freqs[2] += diatonicWheel[wheelIdx].os
+                freqs[1] = freqs[2] - Number(nextIntervals[2])
+                freqs[0] = freqs[1] - Number(nextIntervals[0])
+                break
+            }
+            case '2nd inversion': {
+                freqs[1] += diatonicWheel[wheelIdx].os
+                freqs[0] = freqs[1] - Number(nextIntervals[0])
+                freqs[2] = freqs[1] + Number(nextIntervals[2])
+                break
+            }
+        }
+        // only works for root position shifts right now
+    } else {
+        // same triad, inverted up
+        let lp = freqs[0] // low pitch
+        freqs[0] = freqs[1]
+        freqs[1] = freqs[2]
+        freqs[2] = lp + 12
+    }
 
     if (lat) {
-        shiftTriad(freqs, 0)
+        let ok = shiftTriad(freqs, 0)
+        if (!ok) {
+            // alert('breka on thru up')
+            wheelIdx--
+        }
         return
     }
     shiftTriad(freqs, 1)
@@ -393,17 +473,75 @@ function invertTriadUp(lat = false) {
 
 function invertTriadDown(lat = false) {
     let freqs = []
-    tri.forEach((div) => {
-        freqs.push(div.abs)
+    tri.forEach((note) => {
+        freqs.push(note.abs)
     })
 
-    let hp = freqs[2] // high pitch
-    freqs[2] = freqs[1]
-    freqs[1] = freqs[0]
-    freqs[0] = hp - 12
+    if (lat && keyMode) {
+        if (wheelIdx - 1 < 0) {
+            wheelIdx = diatonicWheel.length - 1
+        } else {
+            wheelIdx--
+        }
 
+        let nextQuality = diatonicWheel[wheelIdx].q
+        let nextIntervals
+        for (let [k, v] of triadMap) {
+            if (v.inversion == inversion && v.quality == nextQuality) {
+                nextIntervals = k
+                break
+            }
+        }
+        if (nextIntervals == null) {
+            glow()
+            return
+        }
+
+        // need next offset value when going backwards
+        let offset
+        if (wheelIdx == diatonicWheel.length - 1) {
+            offset = diatonicWheel[0].os
+        } else {
+            offset = diatonicWheel[wheelIdx + 1].os
+        }
+
+        switch (inversion) {
+            case 'root position': {
+                freqs[0] -= offset
+                freqs[1] = freqs[0] + Number(nextIntervals[0])
+                freqs[2] = freqs[1] + Number(nextIntervals[2])
+                break
+            }
+            case '1st inversion': {
+                freqs[2] -= offset
+                freqs[1] = freqs[2] - Number(nextIntervals[2])
+                freqs[0] = freqs[1] - Number(nextIntervals[0])
+                break
+            }
+            case '2nd inversion': {
+                freqs[1] -= offset
+                freqs[0] = freqs[1] - Number(nextIntervals[0])
+                freqs[2] = freqs[1] + Number(nextIntervals[2])
+                break
+            }
+        }
+    } else {
+        // same triad, inverted down
+        let hp = freqs[2] // high pitch
+        freqs[2] = freqs[1]
+        freqs[1] = freqs[0]
+        freqs[0] = hp - 12
+    }
     if (lat) {
-        shiftTriad(freqs, 0)
+        let ok = shiftTriad(freqs, 0)
+        if (!ok) {
+            // alert('breka on thru down')
+            if (wheelIdx == diatonicWheel.length - 1) {
+                wheelIdx = 0
+            } else {
+                wheelIdx++
+            }
+        }
         return
     }
     shiftTriad(freqs, -1)
@@ -416,6 +554,7 @@ const dimBtn = document.getElementById('dim')
 const augBtn = document.getElementById('aug')
 
 const buttons = document.getElementById('buttons')
+
 // delegate button click actions
 buttons.addEventListener('click', (e) => {
     if (tri.length < 1) {
@@ -430,8 +569,8 @@ buttons.addEventListener('click', (e) => {
     const thirdIdx = tri.indexOf(third)
     const fifthIdx = tri.indexOf(fifth)
 
-    clearStyle()
-    let ok = true
+    clearStyle() // pre-emptive
+    let ok = false
     switch (buttID) {
         case quality: {
             glow()
@@ -443,14 +582,22 @@ buttons.addEventListener('click', (e) => {
         }
         case 'min': {
             ok = toMin(thirdIdx, fifthIdx)
+            if (keyMode) toggleKeyMode()
+            // capturedRoot = null -- cleaner way to GET OUT of key MODE???
             break
         }
         case 'dim': {
             ok = toDim(thirdIdx, fifthIdx)
+            if (keyMode) toggleKeyMode()
             break
         }
         case 'aug': {
             ok = toAug(thirdIdx, fifthIdx)
+            if (keyMode) toggleKeyMode()
+            break
+        }
+        case 'key-mode': {
+            toggleKeyMode()
         }
     }
     if (ok) {
@@ -490,7 +637,6 @@ function toMin(thirdIdx, fifthIdx) {
 }
 
 function toDim(thirdIdx, fifthIdx) {
-    // alert(qual)
     if (quality == 'maj' && fifth.previousElementSibling) {
         tri[thirdIdx] = third.previousElementSibling
         tri[fifthIdx] = fifth.previousElementSibling
@@ -499,7 +645,6 @@ function toDim(thirdIdx, fifthIdx) {
         third.previousElementSibling &&
         fifth.previousElementSibling?.previousElementSibling
     ) {
-        console.log('crazy')
         tri[fifthIdx] = fifth.previousElementSibling.previousElementSibling
         tri[thirdIdx] = tri[thirdIdx].previousElementSibling
     } else if (quality == 'min' && fifth.previousElementSibling) {
@@ -529,6 +674,7 @@ function toAug(thirdIdx, fifthIdx) {
     return true
 }
 
+// really only useful when current triad is neutral (like F#/Gb and key of C)
 function signButtonNameToggle(e) {
     if (rootAlpha == undefined) {
         return
@@ -538,102 +684,128 @@ function signButtonNameToggle(e) {
     } else {
         e.target.innerText = '♭'
     }
-    switchSigns()
+    switchSignsRedux()
     calcTriadType()
 }
 
 const qualityBtnz = [minBtn, majBtn, dimBtn, augBtn]
 
 function updateName() {
-    triadInfo.innerHTML = `<b>${rootAlpha} ${quality}</b> <i>${inversion}</i>`
+    if (capturedRoot) {
+        triadInfo.innerHTML = `<div id='key-hud'>Key of ${capturedRoot}</div>  
+			<div><b>${rootAlpha} ${quality}</b></div>
+			<div><i>${inversion}</i></div>`
+    } else {
+        triadInfo.innerHTML = `
+		<p class="ghost"></p>
+			<p><b>${rootAlpha} ${quality}</b><i>${inversion}</i></p>
+			<p class='ghost'></p>`
+    }
 }
 
-function updateDash() {
+function updateUI() {
     updateName()
     triadInfo.classList.add('fade-in')
 
     for (let s of qualityBtnz) {
         s.classList.remove('active')
     }
-    switch (quality) {
-        case 'maj':
-            majBtn.classList.add('active')
-            break
-        case 'min':
-            minBtn.classList.add('active')
-            break
-        case 'dim':
-            dimBtn.classList.add('active')
-            break
-        case 'aug':
-            augBtn.classList.add('active')
+    if (keyMode) keyModeButton.classList.add('visible', 'active')
+    else {
+        keyModeButton.classList.remove('visible', 'active')
+        switch (quality) {
+            case 'maj':
+                majBtn.classList.add('active')
+                keyModeButton.classList.add('visible')
+                break
+            case 'min':
+                minBtn.classList.add('active')
+                break
+            case 'dim':
+                dimBtn.classList.add('active')
+                break
+            case 'aug':
+                augBtn.classList.add('active')
+        }
     }
 }
 
-const upButton = document.getElementById('up')
-upButton.addEventListener('click', () => {
-    if (tri.length > 1) {
-        invertTriadUp()
-    }
-})
+const dpad = document.getElementById('dpad')
 
-const downButton = document.getElementById('down')
-downButton.addEventListener('click', () => {
-    if (tri.length > 1) {
-        invertTriadDown()
+dpad.addEventListener('click', (e) => {
+    if (tri.length < 1) {
+        displayWarning()
+        return
     }
-})
-
-const upLatBtn = document.getElementById('up-lat')
-upLatBtn.addEventListener('click', () => {
-    if (tri.length > 1) {
-        invertTriadUp(true)
-    }
-})
-
-const downLatBtn = document.getElementById('down-lat')
-downLatBtn.addEventListener('click', () => {
-    if (tri.length > 1) {
-        invertTriadDown(true)
+    let dirBtn = e.target.id
+    switch (dirBtn) {
+        case 'up':
+            invertTriadUp()
+            break
+        case 'down':
+            invertTriadDown()
+            break
+        case 'up-lat':
+            invertTriadUp(true)
+            break
+        case 'down-lat':
+            invertTriadDown(true)
     }
 })
 
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowUp' || event.key === 'k') {
-        if (tri.length > 1) {
+    if (tri.length < 1) {
+        return
+    }
+    switch (event.key) {
+        case 'ArrowUp':
+        case 'k': {
             invertTriadUp()
+            break
         }
-    }
-    if (event.key === 'ArrowDown' || event.key === 'j') {
-        if (tri.length > 1) {
+        case 'ArrowDown':
+        case 'j': {
             invertTriadDown()
+            break
         }
-    }
-    if (event.key === 'ArrowRight' || event.key === 'l') {
-        if (tri.length > 1) {
+        case 'ArrowRight':
+        case 'l': {
             invertTriadUp(true)
+            break
         }
-    }
-    if (event.key === 'ArrowLeft' || event.key === 'h') {
-        if (tri.length > 1) {
+        case 'ArrowLeft':
+        case 'h': {
             invertTriadDown(true)
+            break
         }
     }
 })
 
-function displayWarning() {
-    if (document.querySelector('.select-warn') == null) {
-        const warning = document.createElement('p')
-        warning.className = 'select-warn'
-        warning.innerText = 'please select a note first'
-        let lc = dash.lastElementChild
-        dash.appendChild(warning, lc)
-
-        setTimeout(() => {
-            warning.remove()
-        }, 2000)
+function toggleKeyMode() {
+    keyMode = !keyMode
+    if (keyMode) {
+        capturedRoot = rootAlpha
+        wheelIdx = 0
+    } else {
+        capturedRoot = null
     }
+    keyModeButton.classList.toggle('active')
 }
+
+function displayWarning() {
+    if (document.querySelector('.select-warn') !== null) return
+    const warning = document.createElement('h3')
+    warning.className = 'select-warn'
+    warning.innerText = 'please select a note first'
+    let lc = dash.lastElementChild
+    dash.appendChild(warning, lc)
+
+    setTimeout(() => {
+        warning.remove()
+    }, 2000)
+}
+
+console.log(TwelfthRoot(12)) // HARMONIC
 
 buildFretboard('board', mainboard)
 buildFretboard('board2', freeboard)
--- assets/maps.js
+++ assets/maps.js
@@ -1,85 +1,109 @@
-export const triadMap = {
-    '4,3': {
-        quality: 'maj',
-        inversion: 'root position',
-        thirdIdx: 1,
-        fifthIdx: 2,
-    },
-    '3,5': {
-        quality: 'maj',
-        inversion: '1st inversion',
-        thirdIdx: 0,
-        fifthIdx: 1,
-    },
-    '5,4': {
-        quality: 'maj',
-        inversion: '2nd inversion',
-        thirdIdx: 2,
-        fifthIdx: 0,
-    },
-    '3,4': {
-        quality: 'min',
-        inversion: 'root position',
-        thirdIdx: 1,
-        fifthIdx: 2,
-    },
-    '4,5': {
-        quality: 'min',
-        inversion: '1st inversion',
-        thirdIdx: 0,
-        fifthIdx: 1,
-    },
-    '5,3': {
-        quality: 'min',
-        inversion: '2nd inversion',
-        thirdIdx: 2,
-        fifthIdx: 0,
-    },
-    '3,3': {
-        quality: 'dim',
-        inversion: 'root position',
-        thirdIdx: 1,
-        fifthIdx: 2,
-    },
-    '3,6': {
-        quality: 'dim',
-        inversion: '1st inversion',
-        thirdIdx: 0,
-        fifthIdx: 1,
-    },
-    '6,3': {
-        quality: 'dim',
-        inversion: '2nd inversion',
-        thirdIdx: 2,
-        fifthIdx: 0,
-    },
-    '4,4': {
-        quality: 'aug',
-        inversion: '', // none
-        thirdIdx: 1,
-        fifthIdx: 2,
-    },
-}
-
-export const noteMap = new Map([
-    ['A', 5], // #
-    ['A#', 6],
-    ['Bb', 6],
-    ['B', 7], // #
-    ['C', 8],
-    ['C#', 9],
-    ['Db', 9],
-    ['D', 10], //
-    ['D#', 11],
-    ['Eb', 11],
-    ['E', 0], // #
-    ['F', 1], // b
-    ['F#', 2],
-    ['Gb', 2],
-    ['G', 3], // #
-    ['G#', 4],
-    ['Ab', 4],
+export const triadMap = new Map([
+    [
+        '4,3',
+        {
+            quality: 'maj',
+            inversion: 'root position',
+            thirdIdx: 1,
+            fifthIdx: 2,
+        },
+    ],
+    [
+        '3,5',
+        {
+            quality: 'maj',
+            inversion: '1st inversion',
+            thirdIdx: 0,
+            fifthIdx: 1,
+        },
+    ],
+    [
+        '5,4',
+        {
+            quality: 'maj',
+            inversion: '2nd inversion',
+            thirdIdx: 2,
+            fifthIdx: 0,
+        },
+    ],
+    [
+        '3,4',
+        {
+            quality: 'min',
+            inversion: 'root position',
+            thirdIdx: 1,
+            fifthIdx: 2,
+        },
+    ],
+    [
+        '4,5',
+        {
+            quality: 'min',
+            inversion: '1st inversion',
+            thirdIdx: 0,
+            fifthIdx: 1,
+        },
+    ],
+    [
+        '5,3',
+        {
+            quality: 'min',
+            inversion: '2nd inversion',
+            thirdIdx: 2,
+            fifthIdx: 0,
+        },
+    ],
+    [
+        '3,3',
+        {
+            quality: 'dim',
+            inversion: 'root position',
+            thirdIdx: 1,
+            fifthIdx: 2,
+        },
+    ],
+    [
+        '3,6',
+        {
+            quality: 'dim',
+            inversion: '1st inversion',
+            thirdIdx: 0,
+            fifthIdx: 1,
+        },
+    ],
+    [
+        '6,3',
+        {
+            quality: 'dim',
+            inversion: '2nd inversion',
+            thirdIdx: 2,
+            fifthIdx: 0,
+        },
+    ],
+    [
+        '4,4',
+        {
+            quality: 'aug',
+            inversion: '',
+            thirdIdx: 1,
+            fifthIdx: 2,
+        },
+    ],
 ])
+// maj min min maj maj min dim
+//  +0  +2  +2  +1  +2  +2  +2
+//  	w	w	h	w	w 	w
+// could first identify what current triad is
+// keep the inversion type, increment diatonically
+export const diatonicWheel = [
+    { q: 'maj', os: 1 },
+    { q: 'min', os: 2 },
+    { q: 'min', os: 2 },
+    { q: 'maj', os: 1 },
+    { q: 'maj', os: 2 },
+    { q: 'min', os: 2 },
+    { q: 'dim', os: 2 },
+]
 
 export const noteMapFlats = new Map([
     [0, 'E'],
